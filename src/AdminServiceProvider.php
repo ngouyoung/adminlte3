d73cb5c935aeb902716cfa8dd0727f2e0af56bd4
@UyUong <ngouyoung@yahoo.com>
2022-07-07 14:53:36 +0700

    first commit

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laravel\AdminLte3;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dd('hello');
+    }
+}
