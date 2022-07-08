fa533330e59e4db8dd3e13c56a8f131312864e66
@UyUong <ngouyoung@yahoo.com>
2022-07-08 09:52:30 +0700

    first commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-//, 'permission:backend'
 Route::group(['middleware' => ['auth']], function () {
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['permission:backend']], function () {
         Route::group(['prefix' => 'assessments', 'as' => 'assessments.', 'middleware' => 'permission:access'], function () {
@@ -27,13 +15,5 @@ Route::group(['middleware' => ['auth']], function () {
         require __DIR__ . '/Web/Backend/filter.php';
         require __DIR__ . '/Web/Backend/dashboard.php';
     });
-
-//    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 require __DIR__ . '/Web/Error/index.php';
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
